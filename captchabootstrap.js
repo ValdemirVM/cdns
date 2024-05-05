1e91bd87aea5e0b33dfee9df472461431eeeaa5c
Valdemir Vinagre Mendes <vgmfilmes@hotmail.com>
2024-05-05 19:17:02 -0300

    Update captchabootstrap.js

--- captchabootstrap.js
+++ captchabootstrap.js
@@ -2,8 +2,12 @@
 var captchaCSS = `
 <style>
     #captchavgmj .btn-option.active {
-        background-color: #000;
+        background-color: #6c757d;
         color: #fff;
+    }
+    #captchavgmj button{
+        font-size: 12px !important; /* Tamanho de fonte menor */
+        padding: 0.15rem 0.3rem; /* Espaçamento interno menor */
     }
 </style>
 `;
@@ -20,17 +24,39 @@ function criarCaptcha() {
     // Calcular o resultado da operação matemática
     var resultado = num1 + num2;
     
-    // Criar o HTML do CAPTCHA dinâmico com botões Bootstrap 5
+    // Função para embaralhar um array usando o algoritmo de Fisher-Yates (Knuth)
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    // Gerar dois números aleatórios entre 1 e 10 para a operação matemática
+    var num1 = Math.floor(Math.random() * 10) + 1;
+    var num2 = Math.floor(Math.random() * 10) + 1;
+
+    // Calcular o resultado da operação matemática
+    var resultado = num1 + num2;
+
+    // Criar um array com os valores dos botões
+    var opcoes = [resultado - 1, resultado, resultado + 1];
+
+    // Embaralhar o array de opções
+    opcoes = shuffleArray(opcoes);
+
+    // Criar o HTML do CAPTCHA dinâmico com os botões embaralhados
     var captchaHTML = `
-        <h5>${num1} + ${num2} = ?</h5>
-        <div class="btn-group" role="group" aria-label="Opções" style="width:180px;">
-            <button type="button" class="btn btn-outline-dark btn-option me-1" data-valor="${resultado - 1}">${resultado - 1}</button>
-            <button type="button" class="btn btn-outline-dark btn-option me-1" data-valor="${resultado}">${resultado}</button>
-            <button type="button" class="btn btn-outline-dark btn-option me-1" data-valor="${resultado + 1}">${resultado + 1}</button>
+        <h6 class="text-secondary">${num1} + ${num2} = ?</h6>
+        <div class="btn-group" role="group" aria-label="Opções" style="width:140px;">
+            <button type="button" class="btn btn-outline-secondary btn-option me-1" data-valor="${opcoes[0]}">${opcoes[0]}</button>
+            <button type="button" class="btn btn-outline-secondary btn-option me-1" data-valor="${opcoes[1]}">${opcoes[1]}</button>
+            <button type="button" class="btn btn-outline-secondary btn-option me-1" data-valor="${opcoes[2]}">${opcoes[2]}</button>
         </div>
-        <div id="resultado" class="mt-2"><i class="fa-solid fa-user-shield"></i></div>
+        <small id="resultado" class="mt-2"><i class="fa-solid fa-user-shield text-secondary"></i></small>
     `;
-    
+        
     document.getElementById("captchavgmj").classList.add('border');
     document.getElementById("captchavgmj").classList.add('border-1');
     document.getElementById("captchavgmj").classList.add('border-secondary');
@@ -43,8 +69,8 @@ function criarCaptcha() {
     // Criar o input hidden para armazenar a resposta correta
     var respostaInput = document.createElement('input');
     respostaInput.type = 'hidden';
-    respostaInput.id = 'captchavgmj';
-    respostaInput.name = 'captchavgmj';
+    respostaInput.id = 'captchapost';
+    respostaInput.name = 'captchapost';
     respostaInput.value = '';
 
     // Inserir o input hidden dentro do captcha
@@ -65,7 +91,7 @@ function criarCaptcha() {
             // Verificar se a opção selecionada está correta
             var selectedOption = parseInt(this.getAttribute('data-valor'));
             var resultadoDiv = document.getElementById("resultado");
-            var respostaInput = document.getElementById("captchavgmj");
+            var respostaInput = document.getElementById("captchapost");
 
             //Linguagem
             if (navigator.language === "pt-BR") {
